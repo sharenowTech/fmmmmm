c9f16bd3d10dd5c1bf3273adb73e361599dac13f
Oliver Sabiniarz <sabiniao@informatik.hu-berlin.de>
2017-10-23 10:32:56 +0200

    port data from fmm-int to fmm-hackathon

---
+++ HackathonPortation.py
@@ -0,0 +1,39 @@
+import random
+import DataPortation as port
+from Utilities import get_address
+from Connection import client_int, client_hack
+
+
+def random_coordinates_from_hackathon_location():
+    minlat, maxlat = 40.7497, 40.7687
+    minlon, maxlon = -73.9916, -73.9656
+    lat = str(random.uniform(minlat, maxlat))
+    lon = str(random.uniform(minlon, maxlon))
+    address = get_address(lat, lon)
+
+    return {
+        'lat': lat,
+        'latitude': lat,
+        'lon': lon,
+        'longitude': lon,
+        'full_address': address,
+        'address': address
+    }
+
+
+def import_e_smart_into_hackathon_location(license_plate: str):
+    some_e_smart = port.get_some_e_smart(client_int, 'Stuttgart')
+
+    some_e_smart['plate'] = license_plate
+    some_e_smart['numberPlate'] = license_plate
+    some_e_smart['phone_from_bo'] = '+49 30 233 40 110'
+    some_e_smart['phoneNumber'] = '+49 30 233 40 110'
+    some_e_smart.update(random_coordinates_from_hackathon_location())
+
+    port.import_vehicle(client_hack, 'New Hackshire', some_e_smart)
+
+
+# import 15 vehicles into hackathon location
+if __name__ == '__main__':
+    for i in range(0, 15):
+        import_e_smart_into_hackathon_location('NH-{}'.format(i))
