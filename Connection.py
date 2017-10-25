4f7cfedcb71948745424e439dd5857e1fc9e7a01
Oliver Sabiniarz <sabiniao@informatik.hu-berlin.de>
2017-10-25 16:49:03 +0200

    included accessToken-generation for fmm-api-access

--- Connection.py
+++ Connection.py
@@ -1,5 +1,7 @@
-from config import fmm_int_config, fmm_hack_config
+import requests
+from config import fmm_int_config, fmm_hack_config, fmm_auth_token_config
 from pymongo import MongoClient
+
 
 client_int = MongoClient(
     host=fmm_int_config['host'],
@@ -24,3 +26,11 @@ client_hack = MongoClient(
 
 common_hack = client_hack.get_database('common')
 fmm_hack = client_hack.get_database('fmm')
+
+api_credentials = requests.post(
+    fmm_auth_token_config['url'],
+    json=fmm_auth_token_config['json']
+).json()['serviceResponse']['payLoadData']['authorization']
+
+userId = api_credentials['userId']
+accessToken = api_credentials['accessToken']
