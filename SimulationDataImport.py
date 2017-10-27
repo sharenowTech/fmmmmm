fffbe479424eb741adf551da97e310888e431877
Oliver Sabiniarz <sabiniao@informatik.hu-berlin.de>
2017-10-27 10:40:37 +0200

    removal of sensitive data and charging stations

--- SimulationDataImport.py
+++ SimulationDataImport.py
@@ -1,5 +1,6 @@
 import random
 import FMMFramework as FMM
+import SimulationFramework as sim
 from Utilities import get_address
 from Connection import client_int, client_hack
 from Projection import minlat_, minlon_, maxlat_, maxlon_, hackathon_projector
@@ -23,22 +24,65 @@ def random_coordinates_from_hackathon_location():
 def import_e_smart_into_hackathon_location(license_plate: str):
     some_e_smart = FMM.get_some_e_smart(client_int, 'Stuttgart')
 
-    some_e_smart['plate'] = license_plate
-    some_e_smart['numberPlate'] = license_plate
-    some_e_smart['phone_from_bo'] = '+49 30 233 40 110'
-    some_e_smart['phoneNumber'] = '+49 30 233 40 110'
-
     some_e_smart.update(random_coordinates_from_hackathon_location())
 
     FMM.import_vehicle(client_hack, 'New Hackshire', some_e_smart)
 
+    remove_sensitive_vehicle_data(some_e_smart['_id'], license_plate)
+
+
+def remove_sensitive_vehicle_data(vehicle_id, license_plate: str):
+    vehicle = FMM.get_vehicle_by_id(client_hack, vehicle_id)
+
+    vehicle.update(
+        {
+            'plate': license_plate,
+            'numberPlate': license_plate,
+            'vin': license_plate,
+            'exterior': 'GOOD',
+            'interior': 'GOOD',
+            'exteriorCleanliness': [],
+            'interiorCleanliness': [],
+            'statusChangeReason': '',
+            'ip_from_bo': '0.0.0.0',
+            'has_damages': 'false',
+            'hasDamages': 'false',
+            'ipAdress': '0.0.0.0',
+            'phone_from_bo': '+49 30 233 40 110',
+            'phoneNumber': '+49 30 233 40 110',
+            'remarks': '',
+            'mileage': "0",
+            'operationStateComment': '',
+            'initialRegistrationDate': '2017-10-27',
+            'smartMXId': license_plate,
+            'iccid': license_plate,
+            'rfidVehicleKey': license_plate,
+            'rfidFuelcard': license_plate,
+            'rfidParkingcard': license_plate,
+        }
+    )
+
+    FMM.get_vehicle_collection(client_hack).replace_one(
+        {'_id': vehicle_id},
+        vehicle
+    )
+
+
+def import_charging_station(charging_station: str, lon_lat: dict):
+    station = FMM.get_some_e_smart(client_int, 'Stuttgart')
+    station['plate'] = charging_station
+    station['numberPlate'] = charging_station
+    station['locationAlias'] = 'newhack'
+    station['status'] = 'blue'
+    station.update(lon_lat)
+    FMM.import_vehicle(client_hack, 'New Hackshire', station)
+    remove_sensitive_vehicle_data(FMM.get_vehicle_by_license_plate(
+        client_hack, charging_station
+    )['_id'], charging_station)
 
 
 if __name__ == '__main__':
-    for i in range(0, 15):
-        # import 15 vehicles into hackathon location
-        # import_e_smart_into_hackathon_location('NH-{}'.format(i))
-        pass
-
+    for i in range(10):
+        sim.create_charging_station_task('charging station {}'.format(i))
 
 
