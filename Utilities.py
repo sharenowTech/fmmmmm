69df6d26c51e5db51f9ecbdd84f66e3fad463185
Oliver Sabiniarz <sabiniao@informatik.hu-berlin.de>
2017-10-28 22:12:37 +0200

    added function creating a dict with addresses from lat lon

--- Utilities.py
+++ Utilities.py
@@ -17,6 +17,20 @@ def get_address(lat, lon) -> str:
 
     return response.json()['display_name']
 
+def expand_dict_with_address(lat_lon_dict: dict) -> dict:
+    lat = lat_lon_dict['lat']
+    lon = lat_lon_dict['lon']
+    address = get_address(lat, lon)
+
+    return {
+        'lat': lat,
+        'lon': lon,
+        'latitude': lat,
+        'longitude': lon,
+        'address': address,
+        'full_address': address
+    }
+
 # tests
 if __name__ == '__main__':
     print(get_address(lat="40.7834104", lon="-73.9549902"))
