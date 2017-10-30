137e195b20b5684d9f1179713e3f5b4e1f9bbb48
Oliver Sabiniarz <o_sabiniarz@yahoo.de>
2017-10-31 00:17:16 +0100

    more concurrency

--- MoveVehicles.py
+++ MoveVehicles.py
@@ -5,31 +5,34 @@ import SimulationFramework as sim
 import random
 import threading
 import queue
+import time
+from config import vehicle_count
 from Connection import client_hack
 from Utilities import expand_dict_with_address
-from time import sleep
 
 
-e = xml.etree.ElementTree.parse('map_hackathon_final.osm').getroot()
-all_nodes = {
+map = xml.etree.ElementTree.parse('map_hackathon_final.osm').getroot()
+nodes = {
     node.get('id'): {
         'lat': node.get('lat'), 'lon': node.get('lon'),
         'latitude': node.get('lat'), 'longitude': node.get('lon')
     }
-    for node in e.findall('node')
+    for node in map.findall('node')
 }
+
+ways = [
+    [
+        nodes[node.get('ref')]
+        for node in way.findall('nd')
+    ]
+    for way in map.findall('way')
+]
+
+plates = list(sim.all_license_plates())
 
 position_queues = {
     plate: queue.LifoQueue() for plate in sim.all_license_plates()
 }
-
-def random_lat_lon_sequence_of_nodes():
-    ways = e.findall('way')
-    random_way = random.choice(ways)
-    return [
-        all_nodes[node.get('ref')]
-        for node in random_way.findall('nd')
-    ]
 
 class QueueWorker(threading.Thread):
     def __init__(self, plate):
@@ -37,31 +40,70 @@ class QueueWorker(threading.Thread):
         self.plate = plate
 
     def run(self):
-        global position_queues
-        if not position_queues[plate].empty():
+        if not position_queues[self.plate].empty():
+            lat_lon = position_queues[self.plate].get()
             client_hack.get_database('fmm').get_collection(
                 'vehicle'
             ).find_one_and_update(
-                filter={'plate': plate},
+                filter={'plate': self.plate},
                 update={
-                    '$set': position_queues[plate].get()
+                    '$set': lat_lon
                 }
             )
-            print('moved {} to {}'.format(plate, lat_lon))
-            position_queues[plate] = queue.LifoQueue()
+            print('moved {} to {}'.format(self.plate, lat_lon))
+            position_queues[self.plate] = queue.LifoQueue()
 
 
+class PositionProducer(threading.Thread):
+    def __init__(self, plate, way):
+        threading.Thread.__init__(self)
+        self.plate = plate
+        self.way = way
+
+    def run(self):
+        for lat_lon in self.way:
+            if minlat_ < float(lat_lon['lat']) < maxlat_ and minlon_ < float(lat_lon['lon']) < maxlon_:
+                position_queues[self.plate].put(lat_lon)
+                QueueWorker(self.plate).start()
+                time.sleep(0.1)
+            else:
+                return
+
+# TODO: every car should run in its own while loop to make it really parallel
+# nice: don't need to join threads then
+
+# TODO: choose a way containing the node the car was located last
 while True:
-    plate = random.choice(list(sim.all_license_plates()))
+    random_ways = {
+        plate: random.choice(ways) for plate in sim.all_license_plates()
+    }
 
-    random_way = random_lat_lon_sequence_of_nodes()
-    for lat_lon in random_way:
-        if minlat_ < float(lat_lon['lat']) < maxlat_ and minlon_ < float(lat_lon['lon']) < maxlon_:
-            position_queues[plate].put(lat_lon)
-            QueueWorker(plate).start()
-            sleep(.1)
-        else:
-            break
+    producers = {
+        plate: PositionProducer(plate, random_ways[plate])
+        for plate in sim.all_license_plates()
+    }
+
+    for plate in sim.all_license_plates():
+        producers[plate].start()
+
+    for plate in sim.all_license_plates():
+        producers[plate].join()
+
+
+
+    """
+    
+    for plate in plates:
+        for lat_lon in random_ways[plate]:
+            if minlat_ < float(lat_lon['lat']) < maxlat_ and minlon_ < float(lat_lon['lon']) < maxlon_:
+                position_queues[plate].put(lat_lon)
+                QueueWorker(plate).start()
+                print('{} seconds passed after last step.'.format(t1-t0))
+            else:
+                break
+    """
+
+
 
     """
     threading.Thread(
